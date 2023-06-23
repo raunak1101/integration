88113b93b826798a3cf306f8df1f5b41a42a229d
raunak1101 <134578765+raunak1101@users.noreply.github.com>
2023-06-23 21:33:17 +0530

    Update new.cpp

--- new.cpp
+++ new.cpp
@@ -1,219 +1,70 @@
-// A C++ program for Bellman-Ford's single source
-// shortest path algorithm.
+#include
 #include <bits/stdc++.h>
 using namespace std;
 
-void computeLPSArray(char* pat, int M, int* lps);
- 
-// Prints occurrences of txt[] in pat[]
-void KMPSearch(char* pat, char* txt)
-{
-    int M = strlen(pat);
-    int N = strlen(txt);
- 
-    // create lps[] that will hold the longest prefix suffix
-    // values for pattern
-    int lps[M];
- 
-    // Preprocess the pattern (calculate lps[] array)
-    computeLPSArray(pat, M, lps);
- 
-    int i = 0; // index for txt[]
-    int j = 0; // index for pat[]
-    while ((N - i) >= (M - j)) {
-        if (pat[j] == txt[i]) {
-            j++;
-            i++;
-        }
- 
-        if (j == M) {
-            printf("Found pattern at index %d ", i - j);
-            j = lps[j - 1];
-        }
- 
-        // mismatch after j matches
-        else if (i < N && pat[j] != txt[i]) {
-            // Do not match lps[0..lps[j-1]] characters,
-            // they will match anyway
-            if (j != 0)
-                j = lps[j - 1];
-            else
-                i = i + 1;
-        }
-    }
+//Function to sort the array according to smallest value of second element in array
+static bool Compare(vector<int>& a, vector<int>& b){
+        return a[1]<b[1];
 }
- 
-// Fills lps[] for given pattern pat[0..M-1]
-void computeLPSArray(char* pat, int M, int* lps)
-{
-    // length of the previous longest prefix suffix
-    int len = 0;
- 
-    lps[0] = 0; // lps[0] is always 0
- 
-    // the loop calculates lps[i] for i = 1 to M-1
-    int i = 1;
-    while (i < M) {
-        if (pat[i] == pat[len]) {
-            len++;
-            lps[i] = len;
-            i++;
-        }
-        else // (pat[i] != pat[len])
-        {
-            // This is tricky. Consider the example.
-            // AAACAAAA and i = 7. The idea is similar
-            // to search step.
-            if (len != 0) {
-                len = lps[len - 1];
- 
-                // Also, note that we do not increment
-                // i here
+
+//Function to print total activities
+void printActivties(vector<pair<int,int>>& arr){
+            cout<<"Activities that a single Person Can Select Are : "<<endl;
+            for(int i=0;i<arr.size();i++){
+                cout<<arr[i].first<<","<<arr[i].second<<endl;
             }
-            else // if (len == 0)
-            {
-                lps[i] = 0;
-                i++;
+            cout<<endl;
+}
+
+//Function to select activities
+int activitySelection(vector<int> start, vector<int> end, int n,vector<pair<int,int>>& activties)
+{
+        //Storing start and end array in a 2d array
+        vector<vector<int>> arr;
+        for(int i=0;i<n;i++){
+            arr.push_back({start[i],end[i]});
+        }
+    
+        //Sorting Array according to ending time 
+        sort(arr.begin(),arr.end(),Compare);
+    
+        //There will be atleast one activity
+        int Total_Activity = 1;
+    
+        //Activity that is ending first
+        int endValue = arr[0][1];
+        
+        //Pushing the first activity
+        activties.push_back({arr[0][0],arr[0][1]});
+        
+        for(int i=1;i<n;i++){
+            //if start time of ith activity is greater than ending time of previous taken activity then update ending time, increase the value of activity 
+and store the answer in activities array
+            if(arr[i][0]>endValue){
+                endValue = arr[i][1];
+                Total_Activity++;
+                activties.push_back({arr[i][0],arr[i][1]});
             }
         }
-    }
+        
+        return Total_Activity;
 }
 
-// a structure to represent a weighted edge in graph
-struct Edge {
-	int src, dest, weight;
-};
-
-// a structure to represent a connected, directed and
-// weighted graph
-struct Graph {
-	// V-> Number of vertices, E-> Number of edges
-	int V, E;
-
-	// graph is represented as an array of edges.
-	struct Edge* edge;
-};
-
-// Creates a graph with V vertices and E edges
-struct Graph* createGraph(int V, int E)
-{
-	struct Graph* graph = new Graph;
-	graph->V = V;
-	graph->E = E;
-	graph->edge = new Edge[E];
-	return graph;
-}
-
-// A utility function used to print the solution
-void printArr(int dist[], int n)
-{
-	printf("Vertex Distance from Source\n");
-	for (int i = 0; i < n; ++i)
-		printf("%d \t\t %d\n", i, dist[i]);
-}
-
-// The main function that finds shortest distances from src
-// to all other vertices using Bellman-Ford algorithm. The
-// function also detects negative weight cycle
-void BellmanFord(struct Graph* graph, int src)
-{
-	int V = graph->V;
-	int E = graph->E;
-	int dist[V];
-
-	// Step 1: Initialize distances from src to all other
-	// vertices as INFINITE
-	for (int i = 0; i < V; i++)
-		dist[i] = INT_MAX;
-	dist[src] = 0;
-
-	// Step 2: Relax all edges |V| - 1 times. A simple
-	// shortest path from src to any other vertex can have
-	// at-most |V| - 1 edges
-	for (int i = 1; i <= V - 1; i++) {
-		for (int j = 0; j < E; j++) {
-			int u = graph->edge[j].src;
-			int v = graph->edge[j].dest;
-			int weight = graph->edge[j].weight;
-			if (dist[u] != INT_MAX
-				&& dist[u] + weight < dist[v])
-				dist[v] = dist[u] + weight;
-		}
-	}
-
-	// Step 3: check for negative-weight cycles. The above
-	// step guarantees shortest distances if graph doesn't
-	// contain negative weight cycle. If we get a shorter
-	// path, then there is a cycle.
-	for (int i = 0; i < E; i++) {
-		int u = graph->edge[i].src;
-		int v = graph->edge[i].dest;
-		int weight = graph->edge[i].weight;
-		if (dist[u] != INT_MAX
-			&& dist[u] + weight < dist[v]) {
-			printf("Graph contains negative weight cycle");
-			return; // If negative cycle is detected, simply
-					// return
-		}
-	}
-
-	printArr(dist, V);
-	char txt[] = "ABABDABACDABABCABAB";
-    	char pat[] = "ABABCABAB";
-    	KMPSearch(pat, txt);
-	return;
-}
-
-// Driver's code
-int main()
-{
-	/* Let us create the graph given in above example */
-	int V = 5; // Number of vertices in graph
-	int E = 8; // Number of edges in graph
-	struct Graph* graph = createGraph(V, E);
-
-	// add edge 0-1 (or A-B in above figure)
-	graph->edge[0].src = 0;
-	graph->edge[0].dest = 1;
-	graph->edge[0].weight = -1;
-
-	// add edge 0-2 (or A-C in above figure)
-	graph->edge[1].src = 0;
-	graph->edge[1].dest = 2;
-	graph->edge[1].weight = 4;
-
-	// add edge 1-2 (or B-C in above figure)
-	graph->edge[2].src = 1;
-	graph->edge[2].dest = 2;
-	graph->edge[2].weight = 3;
-
-	// add edge 1-3 (or B-D in above figure)
-	graph->edge[3].src = 1;
-	graph->edge[3].dest = 3;
-	graph->edge[3].weight = 2;
-
-	// add edge 1-4 (or B-E in above figure)
-	graph->edge[4].src = 1;
-	graph->edge[4].dest = 4;
-	graph->edge[4].weight = 2;
-
-	// add edge 3-2 (or D-C in above figure)
-	graph->edge[5].src = 3;
-	graph->edge[5].dest = 2;
-	graph->edge[5].weight = 5;
-
-	// add edge 3-1 (or D-B in above figure)
-	graph->edge[6].src = 3;
-	graph->edge[6].dest = 1;
-	graph->edge[6].weight = 1;
-
-	// add edge 4-3 (or E-D in above figure)
-	graph->edge[7].src = 4;
-	graph->edge[7].dest = 3;
-	graph->edge[7].weight = -3;
-	
-	// Function call
-	BellmanFord(graph, 0);
-
-	return 0;
+int main() {
+    //Adding elements in array of Starting and Ending duration for activity
+    vector<int> start = {10,15,30,45,50,65};
+    vector<int> end =   {20,25,40,60,55,80};
+    
+    //Size of the array
+    int n = start.size();     //Total activites array that a single person can select
+    vector<pair<int,int>> activities;
+    
+    //Funtion Call for finding total activities to select by single person
+    int totalActivity = activitySelection(start,end,n,activities);
+    
+    //Printing the activities that can select
+    cout<<"Total Activities Are : "<< totalActivity <<endl;
+    printActivties(activities);
+    
+    return 0;
 }
